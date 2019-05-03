501d99b5181727fe9a26d2ba703432779c16c01b
leson0301 <48573708+leson0301@users.noreply.github.com>
2019-05-03 12:54:04 +0700

    bt tu lam

---
+++ guiem/js/mywork.js
@@ -0,0 +1,7 @@
+function thu(){
+  let ques = prompt("Ánh Phù Thủy hẹn hò với anh nhé!?","hello Ánh Phù Thủy");
+  let start = (ques == 'yes')?'okay, thế sản xuất cho a một đội bóng đấy.':(ques == 'ok')?'okay, thế sản xuất cho a một đội bóng đấy.':(ques == 'no')?'Anh vẫn trai mặt đấy.':(ques == 'không')?'Anh vẫn trai mặt đấy.':(ques == 'Yes')?'okay, thế sản xuất cho a một đội bóng đấy.':(ques == 'Ok')?'okay, thế sản xuất cho a một đội bóng đấy.':(ques == 'No')?'Anh vẫn trai mặt đấy.':(ques == 'Không')?'Anh vẫn trai mặt đấy.':'i love you.';
+  alert(start);
+}
+
+thu();
